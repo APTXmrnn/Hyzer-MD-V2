900909270ef07978d01aae622c0d431bd744b94c
APTXmrnn <45924038+APTXmrnn@users.noreply.github.com>
2022-04-16 15:11:18 +0700

    Update logololi.js

--- plugins/logololi.js
+++ plugins/logololi.js
@@ -3,7 +3,7 @@ let handler = async (m, { conn, args }) => {
    response = args.join(' ').split('|')
   if (!args[0]) throw 'Masukkan Parameter'
   m.reply('*Wait Ngab*\nProses...')
-  let res = `https://caliphapi.com/api/lolimaker?text=${response[0]}&text2=elyas&apikey=wVLiUzSa`
+  let res = `https://melcanz.com/lolimaker?text=${response[0]}&text2=sumei&apikey=NOVENSxd`
   conn.sendFile(m.chat, res, 'lolilogo.jpg', `Sudah Jadi`, m, false)
 }
 handler.help = ['logololi'].map(v => v + ' <text>')
