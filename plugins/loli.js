3c42bd40ff4f7c27fb2358ff83e283aa64c3f63b
APTXmrnn <45924038+APTXmrnn@users.noreply.github.com>
2022-04-17 22:13:29 +0700

    Update loli.js

--- plugins/loli.js
+++ plugins/loli.js
@@ -1,5 +1,5 @@
 let handler = async (m, { conn }) => {
-    conn.sendButtonImg(m.chat, global.API('lolhuman', '/api/random/loli', {}, 'apikey'), 'Nih', wm, 'NEXT', 'loli', m)
+    conn.sendButtonImg(m.chat, global.API('amel', '/loli', {}, 'apikey'), 'Nih dasar pedofil', wm, 'NEXT', 'loli', m)
 }
 handler.help = ['loli']
 handler.tags = ['anime']
